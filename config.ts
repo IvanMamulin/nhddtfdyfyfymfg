73b71a7592473e19f6d131d771f6876348afd98f
Ivan_Mamulin <101451655+IvanMamulin@users.noreply.github.com>
2023-12-16 19:07:50 +0300

    Initial commit

---
+++ config.ts
@@ -0,0 +1,17 @@
+// import token from './public.pem';
+
+export const window = {
+    width: 900,
+    height: 550,
+    frame: false,
+    resizable: false,
+    maximizable: false,
+    fullscreenable: false,
+    title: 'Aurora Launcher',
+};
+
+export const api = {
+    ws: 'ws://localhost:1370/ws',
+    web: 'http://localhost:1370/',
+    // extraToken: token,
+};
